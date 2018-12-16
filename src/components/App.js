8add72895c928ff35038c8564fc02780e734f0ef
Nana Mochihara <nana.mochihara@gmail.com>
2018-12-16 22:33:41 +0100

    Add react-router in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-export default class App extends React.Component {
-  render() {
-    return <div> hej </div>;
-  }
-}
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+
+        {/* 404 */}
+        <Route>
+          <Fragment>
+            <h1>This is not the page you are looking for</h1>
+            <p>Use the force to find the right page, or type a correct URL</p>
+          </Fragment>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
