0000ee6c40c1c6cc1e037d4a9c69044f2b498b31
Nana Mochihara <nana.mochihara@gmail.com>
2018-12-17 13:30:34 +0100

    Update CreateGroup component

--- src/components/CreateGroup.js
+++ src/components/CreateGroup.js
@@ -1,30 +1,32 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import AddCircleOutline from '@material-ui/icons/AddCircleOutline';
-import Paper from '@material-ui/core/Paper';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 import Typography from '@material-ui/core/Typography';
 import withStyles from '@material-ui/core/styles/withStyles';
 
 const styles = theme => ({
-  paper: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: `${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px ${theme
-      .spacing.unit * 3}px`
-  },
   addCircle: {
-    marginRight: theme.spacing.unit
+    fontSize: '40px',
+    marginLeft: theme.spacing.unit
   }
 });
 
 const CreateGroup = props => {
   const { classes } = props;
   return (
-    <Paper className={classes.paper}>
-      <AddCircleOutline className={classes.addCircle} />
-      <Typography variant={'subtitle1'}>Create group</Typography>
-    </Paper>
+    <Fragment>
+      <ListItem button>
+        <ListItemIcon>
+          <AddCircleOutline className={classes.addCircle} color="action" />
+        </ListItemIcon>
+        <ListItemText>
+          <Typography variant={'subtitle1'}>Create group</Typography>
+        </ListItemText>
+      </ListItem>
+    </Fragment>
   );
 };
 
