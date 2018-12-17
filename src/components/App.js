5004a357753733fd0ceb638bf8ada8c553364b3b
Nana Mochihara <nana.mochihara@gmail.com>
2018-12-17 21:54:11 +0100

    Add CssBaseline to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import SignUp from './SignUp';
 import Login from './Login';
 import Album from './Album';
 
 export const App = () => {
   return (
+    <Fragment>
+      <CssBaseline />
     <BrowserRouter>
       <Switch>
         <Route path="/signup" component={SignUp} />
@@ -22,5 +25,6 @@ export const App = () => {
         </Route>
       </Switch>
     </BrowserRouter>
+    </Fragment>
   );
 };
