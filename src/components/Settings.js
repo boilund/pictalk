4a46f356b8e9d16b326d95150a42c2291c692019
Nana Mochihara <nana.mochihara@gmail.com>
2018-12-18 15:31:48 +0100

    Add template of five components

---
+++ src/components/Settings.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import withStyles from '@material-ui/core/styles/withStyles';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {};
+
+const Settings = props => {
+  const { classes } = props;
+
+  return (
+    <main>
+      <Typography variant="h6" align="inherit" color="textPrimary" gutterBottom>
+        Settings
+      </Typography>
+    </main>
+  );
+};
+
+Settings.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Settings);
