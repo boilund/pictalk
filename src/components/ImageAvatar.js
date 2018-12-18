4d8943b79221bf76ebb60cb6d3efbb2f1922f8a0
Nana Mochihara <nana.mochihara@gmail.com>
2018-12-18 11:05:49 +0100

    Add large size in ImageAvatar

--- src/components/ImageAvatar.js
+++ src/components/ImageAvatar.js
@@ -7,18 +7,28 @@ import Grid from '@material-ui/core/Grid';
 const styles = theme => ({
   avatar: {
     margin: theme.spacing.unit
+  },
+  Lg: {
+    margin: theme.spacing.unit,
+    width: 100,
+    height: 100
   }
 });
 
 const ImageAvatar = props => {
-  const { classes } = props;
+  const { classes, size } = props;
   return (
-    <Avatar alt="raichu" src="/imgs/raichu.png" className={classes.avatar} />
+    <Avatar
+      alt="raichu"
+      src="/imgs/raichu.png"
+      className={size === 'Lg' ? `${classes.Lg}` : `${classes.avatar}`}
+    />
   );
 };
 
 ImageAvatar.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  size: PropTypes.string
 };
 
 export default withStyles(styles)(ImageAvatar);
