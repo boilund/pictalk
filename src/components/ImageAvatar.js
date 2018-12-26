66924b0a047a06be2f2447980cf40eeb96edbf03
Nana Mochihara <nana.mochihara@gmail.com>
2018-12-26 23:04:57 +0900

    Change default group image

--- src/components/ImageAvatar.js
+++ src/components/ImageAvatar.js
@@ -5,24 +5,26 @@ import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
 
 const styles = theme => ({
-  avatar: {
-    margin: theme.spacing.unit
+  md: {
+    margin: theme.spacing.unit,
+    border: '1px solid lightgray'
   },
   Lg: {
     margin: theme.spacing.unit,
-    width: 100,
-    height: 100
+    width: 90,
+    height: 90,
+    border: '1px solid lightgray'
   }
 });
 
 const ImageAvatar = props => {
-  const { classes, size, key, image } = props;
+  const { classes, size, key, image, name } = props;
   return (
     <Avatar
       key={key}
-      alt="raichu"
-      src={image || '/imgs/raichu.png'}
-      className={size === 'Lg' ? `${classes.Lg}` : `${classes.avatar}`}
+      alt={name ? `${name}` : 'default image'}
+      src={image || '/imgs/default-group.svg'}
+      className={size === 'Lg' ? `${classes.Lg}` : `${classes.md}`}
     />
   );
 };
@@ -31,7 +33,8 @@ ImageAvatar.propTypes = {
   classes: PropTypes.object.isRequired,
   size: PropTypes.string,
   key: PropTypes.string,
-  image: PropTypes.string
+  image: PropTypes.string,
+  name: PropTypes.string
 };
 
 export default withStyles(styles)(ImageAvatar);
