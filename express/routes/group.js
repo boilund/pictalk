c4c90723f45bd4f5207a823a5e0bf9fa70d32b50
Nana Mochihara <nana.mochihara@gmail.com>
2018-12-29 11:31:19 +0900

    Save new group and update member's groups data (exist bug)

--- express/routes/group.js
+++ express/routes/group.js
@@ -2,27 +2,26 @@ const User = require('../classes/User.class');
 const Group = require('../classes/Group.class');
 
 exports.creategroup = (req, res) => {
-  console.log('backend');
-  const { userId, groupname, members } = req.body;
+  const { groupname, members } = req.body;
 
   const newGroup = new Group({
-    userId: userId,
     name: groupname,
     // image:
     members: members,
     open: true
   });
+
   newGroup
     .save()
     .then(group => {
-      User.findOneAndUpdate(
-        { _id: req.params._id },
-        { $push: { groups: group._id } }
-      )
-        .then(() => res.status(200).json({ success: true }))
-        .catch(err => {
-          throw err;
-        });
+      console.log('group', group.members);
+      for (let member of group.members) {
+        User.findOneAndUpdate({ _id: member }, { $push: { groups: group._id } })
+          .then(() => res.status(200).json({ success: true }))
+          .catch(err => {
+            throw err;
+          });
+      }
     })
     .catch(err => {
       throw err;
