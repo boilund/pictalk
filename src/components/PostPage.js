6b9c86b0be9ee7fac40c5e570aac72a29eeab2aa
Nana Mochihara <nana.mochihara@gmail.com>
2019-01-10 11:26:53 +0100

    Create steps for submit a post

--- src/components/PostPage.js
+++ src/components/PostPage.js
@@ -1,21 +1,144 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Header from '../containers/Header';
+import AddPhotoStep from './AddPhotoStep';
 
-const styles = {};
+const styles = theme => ({
+  root: {
+    width: '90%'
+  },
+  backButton: {
+    marginRight: theme.spacing.unit
+  },
+  instructions: {
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit
+  },
+  preview: {
+    width: '300px',
+    height: '300px',
+    backgroundSize: ' cover',
+    backgroundRepeat: ' no-repeat',
+    backgroundPosition: 'center'
+  }
+});
 
-const PostPage = props => {
-  const { classes } = props;
-
-  return (
-    <main>
-      <Typography variant="h6" align="inherit" color="textPrimary" gutterBottom>
-        PostPage
-      </Typography>
-    </main>
-  );
+const getSteps = () => {
+  return ['Select a photo', 'Write description', 'Submit'];
 };
+
+const getStepContent = stepIndex => {
+  switch (stepIndex) {
+    case 0:
+      return <AddPhotoStep />;
+    case 1:
+      return 'What is an ad group anyways?';
+    case 2:
+      return 'This is the bit I really care about!';
+    default:
+      return 'Unknown stepIndex';
+  }
+};
+
+class PostPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeStep: 0
+    };
+  }
+
+  handleNext = () => {
+    this.setState({
+      activeStep: this.state.activeStep + 1
+    });
+  };
+
+  handleBack = () => {
+    this.setState({
+      activeStep: this.state.activeStep - 1
+    });
+  };
+
+  handleReset = () => {
+    this.setState({
+      activeStep: 0
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const steps = getSteps();
+    const { activeStep } = this.state;
+    return (
+      <Fragment>
+        <Header />
+        <main>
+          <Typography
+            variant="h6"
+            align="inherit"
+            color="textPrimary"
+            gutterBottom
+          >
+            Submit photo step
+          </Typography>
+          <div className={classes.root}>
+            <Stepper activeStep={activeStep} alternativeLabel>
+              {steps.map(label => {
+                return (
+                  <Step key={label}>
+                    <StepLabel>{label}</StepLabel>
+                  </Step>
+                );
+              })}
+            </Stepper>
+            <div>
+              {activeStep === steps.length ? (
+                <div>
+                  <Typography className={classes.instructions}>
+                    All steps completed
+                  </Typography>
+                  <Button onClick={() => this.handleReset()}>Reset</Button>
+                </div>
+              ) : (
+                <div>
+                  <Typography className={classes.instructions}>
+                    {getStepContent(activeStep)}
+                  </Typography>
+                  <div>
+                    <Button
+                      disabled={activeStep === 0}
+                      onClick={() => this.handleBack()}
+                      className={classes.backButton}
+                    >
+                      Back
+                    </Button>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={() => this.handleNext()}
+                    >
+                      {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                    </Button>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </main>
+      </Fragment>
+    );
+  }
+}
 
 PostPage.propTypes = {
   classes: PropTypes.object.isRequired
