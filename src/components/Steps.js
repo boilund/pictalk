2f983678e244d999e92a62e0a7392633a6e586b9
Nana Mochihara <nana.mochihara@gmail.com>
2019-01-11 17:07:05 +0100

    Change post page layout

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -1,13 +1,27 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-const styles = {};
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    margin: 'auto',
+    marginTop: theme.spacing.unit * 5,
+    maxWidth: 1000
+  }
+});
 
 const Steps = props => {
   const {
@@ -21,58 +35,64 @@ const Steps = props => {
   } = props;
 
   return (
-    <Fragment>
-      <Typography
-        variant="h6"
-        align="inherit"
-        color="textPrimary"
-        gutterBottom
-        className={classes.instructions}
-      >
-        Submit photo step
-      </Typography>
-      <div className={classes.root}>
-        <Stepper activeStep={activeStep} alternativeLabel>
-          {steps.map(label => {
-            return (
-              <Step key={label}>
-                <StepLabel>{label}</StepLabel>
-              </Step>
-            );
-          })}
-        </Stepper>
-        <div>
-          {activeStep === steps.length ? (
-            <div>
-              <Typography className={classes.instructions}>
-                All steps completed
-              </Typography>
-              <Button onClick={() => handleReset()}>Reset</Button>
-            </div>
-          ) : (
-            <div>
-              {getStepContent(activeStep)}
+    <main className={classes.root}>
+      <Paper className={classes.paper}>
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <Typography
+              variant="h6"
+              align="inherit"
+              color="textPrimary"
+              gutterBottom
+              className={classes.instructions}
+            >
+              Post a picture
+            </Typography>
+            <div className={classes.root}>
+              <Stepper activeStep={activeStep} alternativeLabel>
+                {steps.map(label => {
+                  return (
+                    <Step key={label}>
+                      <StepLabel>{label}</StepLabel>
+                    </Step>
+                  );
+                })}
+              </Stepper>
               <div>
-                <Button
-                  disabled={activeStep === 0}
-                  onClick={() => handleBack()}
-                  className={classes.backButton}
-                >
-                  Back
-                </Button>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={() => handleNext()}
-                >
-                  {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-                </Button>
+                {activeStep === steps.length ? (
+                  <div>
+                    <Typography className={classes.instructions}>
+                      All steps completed
+                    </Typography>
+                    <Button onClick={() => handleReset()}>Reset</Button>
+                  </div>
+                ) : (
+                  <div>
+                    {getStepContent(activeStep)}
+                    <div>
+                      <Button
+                        disabled={activeStep === 0}
+                        onClick={() => handleBack()}
+                        className={classes.backButton}
+                      >
+                        Back
+                      </Button>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => handleNext()}
+                      >
+                        {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                      </Button>
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
-          )}
-        </div>
-      </div>
-    </Fragment>
+          </Grid>
+        </Grid>
+      </Paper>
+    </main>
   );
 };
 
