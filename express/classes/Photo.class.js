d4c61d7c0efe451ddf7fca6693fba96b93f24769
Nana Mochihara <nana.mochihara@gmail.com>
2019-01-13 14:29:30 +0100

    Add file name in photo schema

--- express/classes/Photo.class.js
+++ express/classes/Photo.class.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PhotoSchema = new Schema({
+  filename: String,
   postedGroup: {
     type: Schema.Types.ObjectId,
     ref: 'Group'
