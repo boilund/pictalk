77fb5b15567e3aa9719d0e90035b1d0824b7b978
Nana Mochihara <nana.mochihara@gmail.com>
2019-01-14 11:17:23 +0100

    Update card in album page

---
+++ src/components/PostCard.js
@@ -0,0 +1,129 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import classnames from 'classnames';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import red from '@material-ui/core/colors/red';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+
+const styles = theme => ({
+  card: {
+    marginBottom: theme.spacing.unit * 3,
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%' // 16:9
+  },
+  actions: {
+    display: 'flex'
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest
+    })
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)'
+  },
+  avatar: {
+    backgroundColor: red[500]
+  }
+});
+
+class PostCard extends React.Component {
+  state = { expanded: false };
+
+  handleExpandClick = () => {
+    this.setState({ expanded: !this.state.expanded });
+  };
+
+  render() {
+    const { classes, post } = this.props;
+
+    return (
+      <Card className={classes.card}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="Recipe" className={classes.avatar}>
+              R
+            </Avatar>
+          }
+          action={
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title={post.photographer}
+          subheader={post.date}
+        />
+        <CardMedia
+          className={classes.media}
+          image={`/images/uploads/${post.filename[0]}`}
+          title={post.filename}
+        />
+        <CardContent>
+          <Typography component="p">
+            {post.photographer} {post.description}
+          </Typography>
+        </CardContent>
+        <CardActions className={classes.actions} disableActionSpacing>
+          <IconButton aria-label="Add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="Share">
+            <CameraIcon />
+          </IconButton>
+          <IconButton
+            className={classnames(classes.expand, {
+              [classes.expandOpen]: this.state.expanded
+            })}
+            onClick={this.handleExpandClick}
+            aria-expanded={this.state.expanded}
+            aria-label="Show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>Comment:</Typography>
+            {post.comments.map((comment, i) => (
+              <Typography paragraph key={i}>
+                {comment}
+              </Typography>
+            ))}
+          </CardContent>
+          <CardActions>
+            <Button size="small" color="primary">
+              Add Comment...
+            </Button>
+          </CardActions>
+        </Collapse>
+      </Card>
+    );
+  }
+}
+
+PostCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+  post: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(PostCard);
--- src/containers/Album.js
+++ src/containers/Album.js
@@ -2,11 +2,6 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 import classNames from 'classnames';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Header from './Header';
@@ -14,10 +9,10 @@ import BottomMenuBar from '../components/BottomMenuBar';
 import SideMenu from '../components/SideMenu';
 import CreateGroupDialog from './CreateGroupDialog';
 import Loading from '../components/Loading';
+import PostCard from '../components/PostCard';
 
 import * as actions from '../actions';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
 
 const styles = theme => ({
   layout: {
@@ -32,21 +27,8 @@ const styles = theme => ({
   },
   cardGrid: {
     padding: `${theme.spacing.unit * 8}px 0`
-  },
-  card: {
-    height: 800,
-    display: 'flex',
-    flexDirection: 'column'
-  },
-  cardMedia: {
-    paddingTop: '56.25%' // 16:9
-  },
-  cardContent: {
-    flexGrow: 1
   }
 });
-
-const cards = [1, 2, 3, 4, 5];
 
 class Album extends React.Component {
   componentDidMount() {
@@ -55,7 +37,6 @@ class Album extends React.Component {
 
   render() {
     const { classes, isFetching, user, group, posts } = this.props;
-    console.log('images', posts);
 
     if (isFetching) {
       return <Loading />;
@@ -74,30 +55,11 @@ class Album extends React.Component {
           <div className={classNames(classes.layout, classes.cardGrid)}>
             <Grid container spacing={40}>
               <Grid item xs={12} md={9}>
-                {posts &&
-                  posts.map((post, i) => (
-                    <Card className={classes.card} key={i}>
-                      <CardMedia
-                        className={classes.cardMedia}
-                        image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
-                        title="Image title"
-                      />
-                      <CardContent className={classes.cardContent}>
-                        <Typography gutterBottom variant="h5" component="h2">
-                          Heading
-                        </Typography>
-                        <Typography>{post.description}</Typography>
-                      </CardContent>
-                      <CardActions>
-                        <Button size="small" color="primary">
-                          View
-                        </Button>
-                        <Button size="small" color="primary">
-                          Edit
-                        </Button>
-                      </CardActions>
-                    </Card>
-                  ))}
+                {posts.length ? (
+                  posts.map((post, i) => <PostCard post={post} key={i} />)
+                ) : (
+                  <Typography>Please post something</Typography>
+                )}
               </Grid>
               <SideMenu group={group} />
             </Grid>
@@ -133,9 +95,7 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-const connected = connect(
+export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(withStyles(styles)(Album));
-
-export default withRouter(connected);
