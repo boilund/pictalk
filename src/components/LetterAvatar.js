a86d7c3b35fa33ced38c3b515735e614938006d2
Nana Mochihara <nana.mochihara@gmail.com>
2019-01-18 18:51:20 +0100

    Avoid crash when component doesn't get nickname

--- src/components/LetterAvatar.js
+++ src/components/LetterAvatar.js
@@ -35,8 +35,11 @@ const styles = {
 
 const LetterAvatar = props => {
   const { classes, nickname, color } = props;
-  // make initialLetter
-  const initialLetter = nickname.substring(0, 1);
+  // make initialLetter (default letter is 'A')
+  let initialLetter = 'A';
+  if (nickname) {
+    initialLetter = nickname.substring(0, 1);
+  }
 
   return <Avatar className={classes[color]}>{initialLetter}</Avatar>;
 };
